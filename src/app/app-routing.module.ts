2e14331481e2c3b8353496f9a5ce839b2fd96800
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2025-03-15 11:58:51 -0500

    Remove "_" from folder names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './_components/home/home.component';
-import { NavbarComponent } from './_components/navbar/navbar.component';
-import { ProjectsComponent } from './_components/projects/projects.component';
-import { ProjectDetailsComponent } from './_components/project-details/project-details.component';
-import { PublicationComponent } from './_components/publication/publication.component';
-import { PeopleComponent } from './_components/people/people.component';
-import { AnnouncementsComponent } from './_components/announcements/announcements.component';
-import { AnnouncementDetailsComponent } from './_components/announcement-details/announcement-details.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { PublicationComponent } from './components/publication/publication.component';
+import { PeopleComponent } from './components/people/people.component';
+import { AnnouncementsComponent } from './components/announcements/announcements.component';
+import { AnnouncementDetailsComponent } from './components/announcement-details/announcement-details.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './_components/navbar/navbar.component';
-import { HomeComponent } from './_components/home/home.component';
-import { ProjectsComponent } from './_components/projects/projects.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ProjectCardComponent } from './_components/project-card/project-card.component';
-import { ProjectDetailsComponent } from './_components/project-details/project-details.component';
-import { PublicationComponent } from './_components/publication/publication.component';
-import { PersonCardComponent } from './_components/person-card/person-card.component';
-import { PeopleComponent } from './_components/people/people.component';
-import { AnnouncementsComponent } from './_components/announcements/announcements.component';
-import { AnnouncementDetailsComponent } from './_components/announcement-details/announcement-details.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { PublicationComponent } from './components/publication/publication.component';
+import { PersonCardComponent } from './components/person-card/person-card.component';
+import { PeopleComponent } from './components/people/people.component';
+import { AnnouncementsComponent } from './components/announcements/announcements.component';
+import { AnnouncementDetailsComponent } from './components/announcement-details/announcement-details.component';
 
 @NgModule({
   declarations: [
